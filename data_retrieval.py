d124e82a354342c5b908c3f65a321eb6bc2241e8
Jason Weeks <49565288+ChandlerWeeks@users.noreply.github.com>
2024-06-19 23:19:57 -0500

    updates

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import geopandas as gpd
-import numpy as np
-import json
+import calendar
 
 class data_reciever:
   def __init__(self):
@@ -16,28 +15,24 @@ def get_fips():
   fips = pd.read_csv('./data/state_and_county_fips_master.csv', dtype={'fips': str})
   return fips
 
-def get_mexican_municipalities():
+def get_municipality_ids():
   municipalities = pd.read_csv('./data/mexico.csv')
   return municipalities
 
-def get_mexican_states():
-  states = pd.read_csv('./data/MEX_states.csv')
-  return states
-
-def create_useable_MX_dataframe(data):
-  municipalities = get_mexican_municipalities()
-  data['location'] = np.NaN
+def create_useable_mx_dataframe(data):
+  municipalities = get_municipality_ids()
+  data['location'] = None
 
   for _, state in municipalities.iterrows():
     municipality_name = state['NAME']
     GEO_ID = state['GEO_ID']
     mask = (data['COUNTRY'] == 'MEX') & (data['COUNTY_MUNI'] == municipality_name.lower())
-    #data['location'] = data['location'].astype(str)
     data.loc[mask, 'location'] = GEO_ID
 
   data['date'] = pd.to_datetime(data['date'])
   data['year'] = data['date'].dt.to_period('Y')
-  data['month'] = data['date'].dt.month
+  month_dict = {i: calendar.month_name[i] for i in range(1, 13)}
+  data['month'] = data['date'].dt.month.map(month_dict)
 
   new_data = pd.DataFrame({
     'location': data['location'],
@@ -52,20 +47,20 @@ def create_useable_MX_dataframe(data):
 
   return new_data
 
-def create_useable_US_dataframe(data):
+def create_useable_us_dataframe(data):
   fips = get_fips()
-  data['location'] = np.NaN  # Initialize a new column 'fips' with Null values
+  data['location'] = None  # Initialize a new column 'fips' with Null values
 
   for idx, row in fips.iterrows():
     mask = (data['STATE'] == str(row['state'])) & (data['COUNTY_MUNI'] == str(row['name']).lower()) & (data['COUNTRY'] == 'USA')
-    #data['location'] = data['location'].astype(str)  
     data.loc[mask, 'location'] = row['fips'] 
 
   data = data.dropna(subset=['location'])  
 
   data['date'] = pd.to_datetime(data['date'])
   data['year'] = data['date'].dt.to_period('Y')
-  data['month'] = data['date'].dt.month
+  month_dict = {i: calendar.month_name[i] for i in range(1, 13)}
+  data['month'] = data['date'].dt.month.map(month_dict)
 
   new_data = pd.DataFrame({
     'location': data['location'],
@@ -81,19 +76,19 @@ def create_useable_US_dataframe(data):
 
 def aggregate_cases(data):
   data['cases'] = 1
-  aggregaged_data = data.groupby(['location', 'name', 'year']).sum().reset_index()
-  aggregaged_data = aggregaged_data.sort_values(by=['year', 'month'])
-  aggregaged_data = aggregaged_data.dropna(subset=['location'])
-
-  print(aggregaged_data)
+  aggregated_data = data.groupby(['location', 'year', 'month']).sum().reset_index()
+  aggregated_data = aggregated_data.sort_values(by=['year', 'month'])
+  aggregated_data = aggregated_data.dropna(subset=['location'])
   
-  return aggregaged_data
+  return aggregated_data
 
 # merges US counties and Mexican municipalities
 def get_locations(data):
-  us_data = create_useable_US_dataframe(data)
-  mx_data = create_useable_MX_dataframe(data)
+  us_data = create_useable_us_dataframe(data)
+  mx_data = create_useable_mx_dataframe(data)
 
   data = pd.concat([us_data, mx_data])
   aggregated_cases = aggregate_cases(data)
+
+  print(aggregated_cases)
   return aggregated_cases
--- visualization.py
+++ visualization.py
@@ -1,11 +1,13 @@
 import plotly.express as px
-import geopandas as gpd
 import pandas as pd
 from urllib.request import urlopen
 import json
 from data_retrieval import *
 
 pd.options.mode.chained_assignment = None
+
+#TODO: Convert the graph to a plotly go graph, add month filter and reenable the years filter, then publish
+#TODO: https://plotly.com/python/choropleth-maps/
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
   geo = json.load(response)
@@ -41,24 +43,65 @@ class data_visualizer:
 
   def plot_data(self):
     fig = px.choropleth(
-              self.df,
-              geojson=geo,
-              locations='location',
-              color='cases',
-              #color_continuous_scale="Reds",
-              scope="north america",
-              hover_data={'location': False, 'name': True, 'cases': True},
-              labels={'cases':'cases',},
-              animation_frame='year',
-              range_color=[self.df['cases'].min(), self.df['cases'].max()],
-              )
-
-    fig.update_layout(
-      title='Vesicular Stomatitis Cases By Year',
-      title_x=0.5,
-      title_font=dict(size=36, color='black', family='Arial, sans-serif', weight='bold'),
-      #margin={"r":0,"t":30,"l":0,"b":0}
+      self.df,
+      geojson=geo,
+      locations='location',
+      color='cases',
+      scope="north america",
+      hover_data={'location': False, 'name': True, 'cases': True},
+      labels={'cases':'cases',},
+      range_color=[self.df['cases'].min(), self.df['cases'].max()],
+    )
+  
+    # Generate a dropdown item for each month
+    months = ["January", "February", "March", "April", "May", "June", 
+          "July", "August", "September", "October", "November", "December"]
+    dropdown_items = []
+    for i, month in enumerate(months):
+      dropdown_items.append(
+        dict(
+          args=[{"transforms[0].value": i + 1}],  # The month number
+          label=month,
+          method="update"
+        )
       )
-
+  
+    # Add a dropdown menu to the layout
+    fig.update_layout(
+      updatemenus=[
+        dict(
+          buttons=dropdown_items,
+          direction="down",
+          pad={"r": 10, "t": 10},
+          showactive=True,
+          x=0.1,
+          xanchor="left",
+          y=1.1,
+          yanchor="top"
+        )
+      ],
+      annotations=[
+        dict(
+          text="Select Month:",
+          showarrow=False,
+          x=0.025,  # Adjust these values to position the label
+          y=1.083,  # Adjust these values to position the label
+          xref="paper",
+          yref="paper"
+        )
+      ]
+    )
+  
+    # Add a filter transform to the data
+    fig.update_layout(
+      transforms=[
+        dict(
+          type='filter',
+          target='date',  # The column to filter
+          operation='month',  # Extract the month from the date
+          value=1  # The initial month to display (January)
+        )
+      ]
+    )
+  
     fig.show()
-
